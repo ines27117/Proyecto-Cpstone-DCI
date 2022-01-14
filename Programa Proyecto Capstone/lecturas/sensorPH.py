98bf9d3e97d778c1ade862019f01c87a9a16abe8
ines27117 <86592924+ines27117@users.noreply.github.com>
2022-01-13 22:41:19 -0600

    Add files via upload

---
+++ Programa Proyecto Capstone/lecturas/sensorPH.py
@@ -0,0 +1,20 @@
+import time
+import board
+import busio
+import adafruit_ads1x15.ads1115 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+
+def obtenerPH():
+    # Se inicializa el canal donde se va a leer el voltaje de la turbidez
+    i2c = busio.I2C(board.SCL, board.SDA)
+    ads = ADS.ADS1115(i2c)
+    channel = AnalogIn(ads, ADS.P1)
+    voltaje = 0.0
+    for x in range(5):
+        voltaje = voltaje + channel.voltage
+        
+    voltaje = voltaje/5
+    voltaje = round(voltaje,2)
+    ph = round((-voltaje + 22.115)/5.7692 , 1)
+#     print("v:",voltaje, "ph:", ph)
+    return ph;
