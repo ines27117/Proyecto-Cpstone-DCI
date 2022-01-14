23e72e9246e5d3169ce2eaabca3ae966700e9237
ZerolCamb <64336142+ZerolCamb@users.noreply.github.com>
2022-01-14 16:12:02 -0600

    Update sensorPH.py

--- Programa Proyecto Capstone/lecturas/sensorPH.py
+++ Programa Proyecto Capstone/lecturas/sensorPH.py
@@ -15,6 +15,7 @@ def obtenerPH():
         
     voltaje = voltaje/5
     voltaje = round(voltaje,2)
+    # Ecuación de la recta
     ph = round((-voltaje + 22.115)/5.7692 , 1)
 #     print("v:",voltaje, "ph:", ph)
     return ph;
